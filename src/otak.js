1a9fcd3e9f23c572e301f8a4d2c870b593524ac6
Beimarsempai <87555802+Beimarsempai@users.noreply.github.com>
2021-07-20 23:33:11 -0400

    Add files via upload

---
+++ src/otak.js
@@ -0,0 +1,73 @@
+const otak = (prefix, pushname) => {
+    return `*Palabras especificas para que el bot interactue con ustedes mis queridos otakus*
+
+
+    Este bot no sirve
+    bañate
+    pack
+    gime
+    Quien es tu sempai?
+    yamete
+    buenos dias
+    A
+    uwu
+    te amo
+    konede
+    ara ara
+    vemos Netflix?
+    Gimeme
+    onichan
+    senpai
+    Ha!
+    sexo
+    Soy gracioso?
+    Diego
+    te importo?
+    No sirves Lore
+    sate sate
+    soy adicto?
+    ¡A
+    Lore no sirves
+    ahder
+    gay
+    vete Lore
+    me gustan los trap
+    roar
+    un negro
+    chistoso
+    que?
+    puedo jugar papu?
+    pero
+    Chau Lore
+    Holi Lore
+    canta Lore
+    Me quieres?
+    El Pepe
+    me conoces Goku?
+    hace frio
+    Di mucho texto XD
+    Que podemos jugar?
+    quieres otro platano?
+    leve el pot..*
+    C corre*
+    Que vez en mi futuro?
+    wenas
+    una salchicha?
+    gao gao
+    dime una groseria
+    i love you
+    el admin
+    me mide 5cm
+    me puedes cantar?
+    Andate
+
+    *Musica favorita de mi creador pon su nombre y escuchala*
+    Beimar
+
+_Ojito escribe tal como esta (mira bien)_
+
+ву Beimar`
+
+}
+
+exports.otak = otak
